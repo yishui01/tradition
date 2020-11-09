72b495336551471a592beaa0d3104aac82870b78
yishui <727040917@qq.com>
2020-11-09 22:06:20 +0800

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,25 +34,6 @@
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
 
-<div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                        </div>
-                    </div><!-- /.container-fluid -->
-                </nav>
-            </div>
-        </div>
-    </div>
 </div>
 
 <!-- Scripts -->
