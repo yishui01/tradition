c802a330abe284590d7393e626e22c62f8fd1ea9
yishui <727040917@qq.com>
2021-01-03 19:07:19 +0800

    fix

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,7 +25,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'         => 'required|between:3,25|unique:users,name,' . Auth::id(),
+            'name'         => 'required|between:1,25|unique:users,name,' . Auth::id(),
             'email'        => 'required|email',
             'introduction' => 'max:80',
             'avatar'       => 'image|dimensions:min_width=208,min_height=208',
