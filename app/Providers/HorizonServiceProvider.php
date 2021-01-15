8ac6459075a10634539cc55da834b3f06165c121
yishui <727040917@qq.com>
2021-01-15 21:55:47 +0800

    fix

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -34,6 +34,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewHorizon', function ($user) {
+
             return in_array($user->email, [
                 //
             ]);
